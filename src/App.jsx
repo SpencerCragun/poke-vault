eb15474c10a88b7aaaeb18372811149d8a97ec00
Spencer Cragun <Spencer.cragun@gmail.com>
2025-01-23 14:21:57 -0800

    add more detail to single card view

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import useFetch from './hooks/useFetch'
 import CardDetails from './components/CardDetails'
 import AllCards from './components/AllCards'
 import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
+      <NavBar />
       <Routes>
+        <Route path='/' element/>
         <Route path="/cards" element={<AllCards />} />
         <Route path='/cards/:id' element={<CardDetails />} />
       </Routes>
--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -18,13 +18,37 @@ export default function CardDetails() {
         alt={data.name}
         style={{ width: '300px' }}
       />
-      <p>Subtype: {data.subtypes?.join(', ')}</p>
+      <p>Subtype: {data.subtypes.join(', ')}</p>
       <p>HP: {data.hp}</p>
-      <p>Types: {data.types?.join(', ')}</p>
+      <p>Types: {data.types.join(', ')}</p>
       <p>
         Average Sell Price: $
-        {data?.cardmarket.prices.averageSellPrice || 'N/A'}
+        {data.cardmarket.prices.averageSellPrice || 'N/A'}
       </p>
+      <h3>Abilities:</h3>
+      <p>
+        {data.abilities.map((ability, index) => (
+          <div key={index} className='ability-parent'>
+          <p>Ability: {ability.name}</p>
+          <p>{ability.text}</p>
+          </div>
+        ))}
+      </p>
+      <h3>Attacks:</h3>
+      <ul>
+        {data.attacks && data.attacks.length > 0 ? (
+          data.attacks.map((attack, index) => (
+            <li key={index} style={{ marginBottom: '1rem' }}>
+              <strong>Name:</strong> {attack.name} <br />
+              <strong>Damage:</strong> {attack.damage} <br />
+              <strong>Cost:</strong> {attack.cost} <br />
+              <strong>Effect:</strong> {attack.text}
+            </li>
+          ))
+        ) : (
+          <p>No attacks available for this card.</p>
+        )}
+      </ul>
     </div>
   )
 }
