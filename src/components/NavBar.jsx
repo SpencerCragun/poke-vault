82dc3553cdf4b6b66929eb2531d3fc9def7d99b9
Spencer Cragun <Spencer.cragun@gmail.com>
2025-01-31 14:27:12 -0800

    more styling, added scrolling js to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from './SearchBar';
 
 export default function NavBar() {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos]);
+
   return (
-    <div className='nav-bar'>
+    <nav className={`nav-bar ${visible ? '' : 'nav-hidden'}`}>
       <div className='nav-left'>
         <h1>PokeVault</h1>
       </div>
@@ -12,6 +25,6 @@ export default function NavBar() {
         <Link to={`/`} className='nav-link'><p>Home</p></Link>
         <Link to={`/sets`} className='nav-link'><p>Sets</p></Link>
       </div>
-    </div>
+    </nav>
   );
 }
