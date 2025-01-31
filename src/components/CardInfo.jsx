a78c6b029cb8d4417c29eeefd4fe3dcf5aa2e55c
Spencer Cragun <Spencer.cragun@gmail.com>
2025-01-31 09:09:29 -0800

    clean up to close branch

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 export default function CardInfo({ name, subtypes, supertypes, hp, types }) {
   return (
     <div className='card-info'>
-      {name && <h1>{name}</h1>}
-      {subtypes && <p>Subtype: {subtypes.join(', ')}</p>}
-      {supertypes && <p>Supertype: {supertypes.join(', ')}</p>}
+      {name && <h1 className='card-name'>{name}</h1>}
+      {subtypes && <p>{subtypes.join(', ')}</p>}
+      {supertypes && <p>{supertypes.join(', ')}</p>}
       {hp && <p>HP: {hp}</p>}
-      {types && <p>Types: {types.join(', ')}</p>}
+      {types && <>
+        <span>Type</span><p>{types.join(', ')}</p>
+      </>}
     </div>
   );
 }
