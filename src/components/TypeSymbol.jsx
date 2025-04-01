1a1e47b94ac6457583a42074ccc0ec35dcb764fc
Spencer Cragun <Spencer.cragun@gmail.com>
2025-04-01 13:52:13 -0700

    change structure of typesymbol page in order to work in deployment

--- src/components/TypeSymbol.jsx
+++ src/components/TypeSymbol.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import colorless from '../assets/colorless.png';
+import darkness from '../assets/darkness.png';
+import dragon from '../assets/dragon.png';
+import fairy from '../assets/fairy.png';
+import fighting from '../assets/fighting.png';
+import fire from '../assets/fire.png';
+import grass from '../assets/grass.png';
+import lightning from '../assets/lightning.png';
+import metal from '../assets/metal.png';
+import psychic from '../assets/psychic.png';
+import water from '../assets/water.png';
 
 export default function TypeSymbol({ type }) {
   const typeSymbols = {
-    Colorless: '/src/assets/colorless.png',
-    Darkness: '/src/assets/darkness.png',
-    Dragon: '/src/assets/dragon.png',
-    Fairy: '/src/assets/fairy.png',
-    Fighting: '/src/assets/fighting.png',
-    Fire: '/src/assets/fire.png',
-    Grass: '/src/assets/grass.png',
-    Lightning: '/src/assets/lightning.png',
-    Metal: '/src/assets/metal.png',
-    Psychic: '/src/assets/psychic.png',
-    Water: '/src/assets/water.png'
+    Colorless: colorless,
+    Darkness: darkness,
+    Dragon: dragon,
+    Fairy: fairy,
+    Fighting: fighting,
+    Fire: fire,
+    Grass: grass,
+    Lightning: lightning,
+    Metal: metal,
+    Psychic: psychic,
+    Water: water
   };
 
   return (
